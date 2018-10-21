cd6ff29f52749ff836d3c1cff83085731293ba77
Dmitry Shorokhov <vip.shoroch@gmail.com>
2018-10-21 13:06:55 +0300

    HZ

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 
 class Config:
-    copter_id = os.environ.get('COPTER_ID')
-    host = os.environ.get('HOST')
+    copter_id = 1  # os.environ.get('COPTER_ID')
+    host = "192.168.43.19"  # os.environ.get('HOST')
     animation_file_path = 'drone{}.csv'.format(copter_id)
