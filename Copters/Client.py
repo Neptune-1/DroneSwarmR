b245cc174bd6086d241ff619bfeb8600bbde8e18
Dmitry Shorokhov <vip.shoroch@gmail.com>
2018-10-21 15:12:09 +0300

    FIX XXX

--- Copters/Client.py
+++ Copters/Client.py
@@ -1,9 +1,14 @@
 import socket
 import csv
 
-from ..config import Config
 from .StateMachine import StateMachine
 from .threads import ServerPollingThread, FlyingThread
+
+
+class Config:
+    copter_id = 1  # os.environ.get('COPTER_ID')
+    host = "192.168.43.19"  # os.environ.get('HOST')
+    animation_file_path = 'drone{}.csv'.format(copter_id)
 
 
 class Client(object):
--- LightmusicClient/Client.py
+++ LightmusicClient/Client.py
@@ -1,8 +1,13 @@
 import socket
 import time
 
-from ..config import Config
 from ..Drone.FlightLib import LedLib
+
+
+class Config:
+    copter_id = 1  # os.environ.get('COPTER_ID')
+    host = "192.168.43.19"  # os.environ.get('HOST')
+    animation_file_path = 'drone{}.csv'.format(copter_id)
 
 
 class Client(object):
